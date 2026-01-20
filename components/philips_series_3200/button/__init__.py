c9417cf1e00459f2230d68e54558e97f8b1a2c9e
TimTomWZ <31109561+TimTomWZ@users.noreply.github.com>
2026-01-20 13:02:11 +0100

    Update __init__.py

--- components/philips_series_3200/button/__init__.py
+++ components/philips_series_3200/button/__init__.py
@@ -1,10 +1,4 @@
-diff --git a/components/philips_series_3200/button/__init__.py b/components/philips_series_3200/button/__init__.py
-index 7777777..8888888 100644
---- a/components/philips_series_3200/button/__init__.py
-+++ b/components/philips_series_3200/button/__init__.py
-@@ -43,7 +43,7 @@ def validate_long_press(config):
- 
- 
+
  CONFIG_SCHEMA = cv.All(
 -    button.BUTTON_SCHEMA.extend(
 +    button.button_schema(ActionButton).extend(
